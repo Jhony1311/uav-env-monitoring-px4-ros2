6eca1b60f7e36c61d8fefa7af32dfeb42dd75d9f
Jhon Ojeda <jhony.ojeda565@gmail.com>
2026-01-15 01:55:40 -0600

    Add setup.py and resource file for uav_offboard_py

--- ros2_ws/src/ros2_ws/src/uav_offboard_py/ros2_ws/src/uav_offboard_py/setup.py
+++ ros2_ws/src/ros2_ws/src/uav_offboard_py/ros2_ws/src/uav_offboard_py/setup.py
@@ -1,1 +1,27 @@
+from setuptools import setup
+
+package_name = 'uav_offboard_py'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Jhonattan Jesus Ojeda Arroyo',
+    maintainer_email='jhony.ojeda565@gmail.com',
+    description='ROS 2 Jazzy PX4 offboard control node for UAV environmental monitoring missions.',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'offboard_mission = uav_offboard_py.offboard_mission:main',
+        ],
+    },
+)
 
